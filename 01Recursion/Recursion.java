1a1687e5f0df5def899c0a1b8407242fadb64d55
Yuxuanzou <yzou2@stuy.edu>
2018-02-03 18:26:47 -0700

    finished the fib method

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -12,12 +12,34 @@ public class Recursion {
         return fact(n - 1) + n;
     }
     
+    public int fib(int n){
+        if (n < 0){
+            throw new IllegalArgumentException();
+        }
+        return helper(0,n,0,1);
+    }
+    
+    public int helper(int sum, int n, int small, int large){
+        int temp = small;
+        if (n == 0){
+            return sum;
+        }
+        else if (n == 1){
+            return large;
+        }
+        return helper(sum = small + large,n - 1, small = large, large + temp);
+    }
+    
+    public double sqrt(double n){
+        
+    }
+    
     public static void main(String[] args){
         Recursion r = new Recursion();
         System.out.println(r.fact(4));
         System.out.println(r.fact(0));
         System.out.println(r.fact(1));
-        System.out.println(r.fact(20));
-        System.out.println(r.fact(-2));
+        System.out.println(r.fib(35));
+        System.out.println(r.fib(-1));
     }
 }
