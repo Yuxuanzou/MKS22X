c9e56cea3a7aa92b988ff26be48901f78a8ccee3
Yuxuanzou <yzou2@stuy.edu>
2018-02-03 19:42:42 -0700

    finished sqrt method

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Recursion {
     public int fact(int n){
         if (n < 0){
@@ -31,7 +32,17 @@ public class Recursion {
     }
     
     public double sqrt(double n){
-        
+        if (n < 0){
+            throw new IllegalArgumentException();
+        }
+        return sqrtHelper(n,n/2);
+    }
+    
+    public double sqrtHelper(double n, double guess){
+        if ((Math.abs((guess) - Math.pow(n,0.5))) < 0.000001){
+            return guess;
+        }
+        return sqrtHelper(n,(n / guess + guess) / 2);
     }
     
     public static void main(String[] args){
@@ -40,6 +51,7 @@ public class Recursion {
         System.out.println(r.fact(0));
         System.out.println(r.fact(1));
         System.out.println(r.fib(35));
-        System.out.println(r.fib(-1));
+        System.out.println(r.sqrt(10.0));
+        System.out.println(r.sqrt(-12));
     }
 }
