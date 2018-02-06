a73df65df67eecc260220931f19b4f2857d28c08
Yuxuanzou <yzou2@stuy.edu>
2018-02-05 21:58:20 -0700

    started the isPossibleSum method but didn't finish

---
+++ test.java
@@ -0,0 +1,12 @@
+ public class test {
+    public boolean isPossibleSum(int n, int target){
+        if (n == 0){
+            return true
+        }
+    }
+     
+    public boolean isPossibleSumHelper(int[] array, int n, int target){
+        if (target == 0){
+            return true;
+        }
+}
