72edd42306b0fd30238e4705ff5ac973847bf1e2
Yuxuanzou <yzou2@stuy.edu>
2018-02-10 23:25:38 -0700

    started the solve method and its helper

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,7 +1,9 @@
 public class QueenBoard{
     private int[][] board;
+    private int size;
 
     public QueenBoard(int size){
+    this.size = size;
 	board = new int[size][size];
         for (int i = 0;i < board.length;i++){
             for (int c = 0;c < board[i].length;c++){
@@ -36,28 +38,29 @@ public class QueenBoard{
     }
 
     private boolean addQueen(int r, int c){
-	if (board[r][c] > 0){
+	if (board[r][c] == 0){
 	    board[r][c] = -1;
+        setThreat(r,c);
 	    return true;
 	}
 	return false;
     }
     
     private void setThreat(int r, int c){
-        for (int col = c;col < board[col].length;col++){
+        if (r > 0 && r < board.length && c < board[r].length){
+        for (int col = c + 1;col < board[r].length;col++){
             board[r][col] += 1;
         }
-        for (int row = r, col = c;row < board.length && col < board[row].length; row++,col++){
+        for (int row = r + 1, col = c + 1;row < board.length && col < board[row].length; row++,col++){
             board[row][col] += 1;
         }
-        for (int row = r, col = c;row < board.length && col < board[row].length; row--,col++){
-            board[row][col] += 1;
         }
     }
 
     private boolean removeQueen(int r, int c){
 	if (board[r][c] == -1){
 	    board[r][c] = 0;
+        removeThreat(r,c);
 	    return true;
 	}
 	return false;
@@ -74,10 +77,31 @@ public class QueenBoard{
             board[row][col] -= 1;
         }
     }
+    
+    public boolean solve(){
+        return solverHelper(size,0,0,0);
+    }
+    
+    public boolean solverHelper(int n,int r, int c, int numOfQueens){
+        if (numOfQueens == n){
+            return true;
+        }
+        for (int col = c; c < board[r].length;c++){
+            if (addQueen(r,col)){
+                return solverHelper(n,r+1,c + 1,numOfQueens + 1);
+            }
+            else {
+                removeQueen(r,col);
+                return solverHelper(n,r, c - 1,numOfQueens - 1)
+            }
+        }
+        return false;
+    }
 
 
     public static void main(String[] args){
-        QueenBoard g = new QueenBoard(4);
+        QueenBoard g = new QueenBoard(10);
+        g.solve();
         System.out.println(g);
     }
 }
