98ce9ead428affa2748f9d9e8bd6ecd3b6b7d84f
Yuxuanzou <yzou2@stuy.edu>
2018-02-15 15:32:25 -0500

    I totally did some useful work

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,5 +1,6 @@
 public class KnightBoard {
     private int[][] board;
+    private int[][] xAndY;
     
     public KnightBoard(int startingRows, int startingCols){
 	if (startingRows < 0 || startingCols < 0){
@@ -12,14 +13,27 @@ public class KnightBoard {
 	String str = "";
 	for (int i = 0;i < board.length;i++){
 	    for (int c = 0;c < board[i].length;c++){
-		if (board[i][c] == 0){
-		    str += "_ ";
+		if (board[i][c] < 10){
+		    if (c == board[i].length){
+			str += " " + board[i][c] + "\n";
+		    }
+		    str += " " + board[i][c];
+		}
+		else {
+		    if (c == board[i].length){
+			str += "" + board[i][c] + "\n";
+		    }
+			str += "" + board[i][c];
 		}
 	    }
 	}
 	return str;
     }
 
+    // public void KnightMove(){
+	
+    // }
+    /*
     public boolean solve(int startingRow, int startingCol){
 	for (int i = 0; i < board.length;i++){
 	    for (int c = 0; c < board[i].length;c++){
@@ -28,9 +42,21 @@ public class KnightBoard {
 		}
 	    }
 	}
+	return solveHelper(startingRow, startingCol, 0);
     }
 
-    public int countSolutions(int startingRow, int startingCol){
+    public boolean solveHelper(int row,int col,int count){
+	if (count => (row * col)){
+	    return true;
+	}
 	
+	}*/
+
+    // public int countSolutions(int startingRow, int startingCol){
+	
+    // }
+    public static void main(String[] args){
+	KnightBoard g = new KnightBoard(10,10);
+	System.out.println(g);
     }
 }
