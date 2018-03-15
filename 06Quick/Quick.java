a14dc4828ab0b98515cde0ecb8cab8df97178086
Yuxuanzou <yzou2@stuy.edu>
2018-03-14 20:14:13 -0400

    finished quickselect, problems with quicksort

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,15 +1,13 @@
 import java.util.*;
 
-public class part{
+public class Quick{
     public static int partition(int[] data,int start,int end){
 	Random rand = new Random();
-	int pivotIndex = rand.nextInt(end - 1);
+	int pivotIndex = rand.nextInt(end) + 1;
 	int pivot = data[pivotIndex];
-	swap(data,pivotIndex,start);
+	swap(data,pivotIndex,0);
 	int small = start + 1;
-	int large = end - 1;
-	System.out.println(pivot);
-	System.out.println(pivotIndex);
+	int large = end;
 	while (small <= large){
 	    if (data[small] <= pivot){
 		small++;
@@ -38,19 +36,41 @@ public class part{
     }
 
     public static int quickselect(int[] data,int k){
-	
+	return quickHelp(data,k,0,data.length - 1);
     }
 
-    public static int helpe(int[] data,int start,int end){
-	if (start >= end){
-	    return 0;
+    public static int quickHelp(int[] data,int k,int start,int end){
+	int pivotIndex = partition(data,start,end);
+        while (pivotIndex != k){
+	    if (pivotIndex > k){
+	        return quickHelp(data,k,start,pivotIndex - 1);
+	    }
+	    if (pivotIndex < k){
+		return quickHelp(data,k,pivotIndex + 1,end);
+	    }
 	}
-	
+	return data[pivotIndex];
     }
+
+    /*public static void quicksort(int[] ary){
+	quicksortHelp(ary,0,ary.length - 1);
+    }
+
+    public static void quicksortHelp(int[] ary,int start,int end){
+        int pivot = partition(ary,start,end);
+        if (start < end){
+	    if (pivot + 1 <= end){
+		quicksortHelp(ary,pivot + 1,end);
+	    }
+	    if (pivot - 1 >= start){
+		quicksortHelp(ary,start,pivot - 1);
+	    }
+	}
+	}
 
     public static void main(String[] args){
 	int[] ary = new int[]{999,999,999,4,1,0,3,2,999,999,999};
-        partition(ary,0,ary.length - 1);
+        quicksort(ary);
 	System.out.println(toString(ary));
-    }
+	}*/
 }
