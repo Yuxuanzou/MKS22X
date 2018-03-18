29e50379de9df9a942a5a15065d91db11412ecb9
Yuxuanzou <yzou2@stuy.edu>
2018-03-18 18:24:55 -0400

    finished quicksort

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -55,21 +55,52 @@ public class Quick{
 	return data[k];
     }
 
-    public static void quicksort(int[] ary){
-	quicksortHelp(ary,0,ary.length - 1);
+    public static void quicksort(int[] data){
+	quicksortHelp(data,0,data.length - 1);
     }
 
-    public static void quicksortHelp(int[] ary,int start,int end){
-        int p = partition(ary,start,end);
-	return;
+    public static void quicksortHelp(int[] data,int start,int end){
+        if (start >= end){
+	    return;
+	}
+	Random rand = new Random();
+	int pivotIndex = rand.nextInt(end - start) + start;
+	int pivot = data[pivotIndex];
+	swap(data,pivotIndex,start);
+	int lt = start;
+	int gt = end;
+	int i = start;
+	while (i <= gt){
+	    if (data[i] == pivot){
+		i++;
+	    }
+	    else if (data[i] > pivot){
+		swap(data,i,gt);
+		gt--;
+	    }
+	    else {
+		swap(data,i,lt);
+		i++;
+		lt++;
+	    }
+	}
+        if (lt > 0){
+	    quicksortHelp(data,start,lt - 1);
+	}
+	quicksortHelp(data,gt + 1,end);
 	}
     
-    public static void main(String[] args){
+    /*public static void main(String[] args){
 	int[] ary = new int[]{999,999,999,4,1,0,3,2,999,999,999,0,-1,32};
-	System.out.println(quickselect(ary,0));
-	//quicksort(ary);
+	//System.out.println(quickselect(ary,0));
+	quicksort(ary);
 	System.out.println(toString(ary));
-    }
-    
+	}*/
+
+    /* public static void main(String[] args){
+	int[] test = {1000, 999,999,999,4,1,0,3,2,999,999,100,100,-10000, 10212, -19212, 23, 12};
+	quicksort(test);
+	System.out.println(toString(test));
+	}*/
 }
 
