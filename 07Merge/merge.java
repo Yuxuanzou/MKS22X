4e5cd758252d3ab12cf5b6484ac4f85a3a13c5dd
Yuxuanzou <yzou2@stuy.edu>
2018-03-21 23:02:06 -0400

    worked on the merge method

--- 07Merge/merge.java
+++ 07Merge/merge.java
@@ -4,6 +4,7 @@ public class merge{
     }
     
     public static void msort(int[] data,int[] temp,int lo,int hi){
+	temp = new int[hi - lo + 1]
 	if (lo >= hi){
 	    return;
 	}
@@ -17,12 +18,16 @@ public class merge{
     }
     
     public static void merge(int[] data,int[] temp,int lo,int mid,int midPlusOne,int hi){
-        int end = hi;
-	int low = lo;
-	for (int i = low + 1;i < mid;i++){
-	    if (temp[i] < temp[i - 1]){
-		
+	int tick = 0;
+        while(lo <= mid && midPlusOne <= hi && tick < temp.length){
+	    if(data[lo] <= data[midPlusOne]){
+		temp[count] = data[lo];
+		lo++;
+		count++;
 	    }
+	    num[count] = data[midPlusOne];
+	    midPlusOne++;
+	    count++
 	}
     }
 
