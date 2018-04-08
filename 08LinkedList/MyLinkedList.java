f34537e67856776d904edebf0765f398898f284a
Yuxuanzou <yzou2@stuy.edu>
2018-04-08 13:26:33 -0600

    changed name

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -2,4 +2,41 @@ public class MyLinkedList{
     public MyLinkedList(){
 	
     }
+
+    private class Node{
+	private Node next,prev;
+	private int data;
+	
+	public Node(int d){
+	    data = d;
+	    next = null;
+	    prev = null;
+	}
+	
+	private Node getNext(){
+	    return next;
+	}
+	
+	private Node getPrev(){
+	    return prev;
+	}
+	
+	private int getValue(){
+	    return data;
+	}
+	
+	private void setNext(Node next){
+	    this.next = next;
+	}
+	
+	private void setPrev(Node prev){
+	    this.prev = prev;
+	}
+	
+	public String toString(){
+	    String str = "";
+	    str += this.getValue();
+	    return str;
+	}
+    }
 }
