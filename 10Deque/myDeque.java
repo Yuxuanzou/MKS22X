d72e202158448d1e2364d6afb7004f546f81cee3
Yuxuanzou <yzou2@stuy.edu>
2018-04-19 11:15:25 -0400

    finish constuctor

--- 10Deque/myDeque.java
+++ 10Deque/myDeque.java
@@ -1,3 +1,23 @@
-public class myDeque{
-    public myDeque 
+import java.util.*;
+
+public class myDeque<E>{
+    private E[] data;
+    
+    @SuppressWarnings("unchecked")
+    public myDeque(){
+	data = (E[])new Object[10];
+    }
+
+    @SuppressWarnings("unchecked")
+    public myDeque(int initialCapacity){
+	if (initialCapacity < 0){
+	    throw new IllegalArgumentException();
+	}
+	data = (E[]) new Object[initialCapacity];
+    }
+
+    public int size(){
+	return data.length;
+    }
+    
 }
